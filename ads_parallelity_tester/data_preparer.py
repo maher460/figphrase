9771ce533ce7d9eeca813bb614d20a260f726703
maher460 <maher460@gmail.com>
2019-09-23 06:52:25 -0400

    wfwefwe

--- ads_parallelity_tester/data_preparer.py
+++ ads_parallelity_tester/data_preparer.py
@@ -27,7 +27,7 @@ keys =  list(w2c_data.keys())
 
 img_keys = img_features.keys()
 
-keys = list(filter(lambda x: x in img_keys, img_features))
+keys = list(filter(lambda x: x in img_keys, keys))
 
 keys.sort()
 
