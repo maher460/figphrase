4395cd23cde77d6ff90eb8a131aa66ef67334f9d
maher460 <maher460@gmail.com>
2019-10-01 08:58:45 -0400

    wefdqwed

--- obj2vec/ad_images_objs_matrix.py
+++ obj2vec/ad_images_objs_matrix.py
@@ -167,7 +167,7 @@ for k in res2.keys():
 
         dists = list(map(lambda m: model.distances(m, labels_model.keys()), mean2))
 
-        smallest_idxs = list(map(lambda d: list(np.argsort(dists)[:1]), dists)) 
+        smallest_idxs = list(map(lambda d: np.argsort(dists)[0], dists)) 
 
         # res3[k] = {labels_model[list(labels_model.keys())[smallest_idx]]}
 
