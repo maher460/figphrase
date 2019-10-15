90d2ad54939e38a3e402ded4beb1138e31e41097
maher460 <maher460@gmail.com>
2019-10-15 09:16:14 -0400

    efwef

--- figpro/evaluation/explore_2_matrix_labels.py
+++ figpro/evaluation/explore_2_matrix_labels.py
@@ -8,13 +8,8 @@ with open("explore2b3_method_matrix.csv", "r") as f:
     lines = csv.reader(f, delimiter=",")
 
     for l in lines:
-    	img = Image.open('/afs/cs/projects/kovashka/maher/vol3/matrix_results_dist_custoff/' + l[0] + ".png")
+        img = Image.open('/afs/cs/projects/kovashka/maher/vol3/matrix_results_dist_custoff/' + l[0] + ".png")
 
-		ImageDraw.Draw(img).text(
-		    (5, 5),  # Coordinates
-		    l[1] + ", " + str(l[3]),  # Text
-		    (0, 0, 0)  # Color
-		)
+        ImageDraw.Draw(img).text((5, 5), l[1] + ", " + str(l[3]), (0, 0, 0))
 
-
-		img.save('/afs/cs/projects/kovashka/maher/vol3/matrix_results_dist_custoff_labels/' + l[0] + ".png")
+        img.save('/afs/cs/projects/kovashka/maher/vol3/matrix_results_dist_custoff_labels/' + l[0] + ".png")
