1495a952de899ba38711b90b26f6b9ddbeed1593
maher460 <maher460@gmail.com>
2019-10-15 09:19:12 -0400

    efwef

--- figpro/evaluation/explore_2_matrix_labels.py
+++ figpro/evaluation/explore_2_matrix_labels.py
@@ -12,4 +12,4 @@ with open("explore2b3_method_matrix.csv", "r") as f:
 
         ImageDraw.Draw(img).text((5, 5), l[1] + ", " + str(l[3]), (0, 0, 0))
 
-        img.save('/afs/cs/projects/kovashka/maher/vol3/matrix_results_dist_custoff_labels/' + l[0] + ".png")
+        img.save('/afs/cs/projects/kovashka/maher/vol3/matrix_results_dist_cutoff_labels/' + l[0] + ".png")
